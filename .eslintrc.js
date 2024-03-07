2e7bfc5517b2f5c974ad66bd9765c0198b3c6e3d
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-03-07 16:41:40 +0000

    https://tanstack.com/query/v5/docs/eslint/eslint-plugin-query

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  extends: ["universe/native", "universe/shared/typescript-analysis"],
+  extends: [
+    "universe/native",
+    "universe/shared/typescript-analysis",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
+  ],
   overrides: [
     {
       files: ["*.ts", "*.tsx", "*.d.ts"],
