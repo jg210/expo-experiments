416478ef3f72e3e5ecf00e0aa55402a203aba887
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-03-07 16:16:49 +0000

    eslint fixes.

--- src/Authorities.tsx
+++ src/Authorities.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 import { FlatList, Text } from "react-native";
 
 interface localAuthorities {
-  localAuthorities: LocalAuthority[]
+  localAuthorities: LocalAuthority[];
 }
 
 interface LocalAuthority {
@@ -13,8 +13,10 @@ interface LocalAuthority {
 
 const queryKey = ["authorities"];
 const getAuthorities: () => Promise<LocalAuthority[]> = async () => {
-  const authorities = await fetch("https://aws.jeremygreen.me.uk/api/fsa/localAuthority");
-  const json = await authorities.json() as unknown as localAuthorities;
+  const authorities = await fetch(
+    "https://aws.jeremygreen.me.uk/api/fsa/localAuthority",
+  );
+  const json = (await authorities.json()) as unknown as localAuthorities;
   return json.localAuthorities;
 };
 
@@ -28,12 +30,12 @@ const Fallback = () => <Text>loading...</Text>;
 export const Authorities = () => {
   const { data } = useSuspenseQuery({ queryKey, queryFn: getAuthorities });
   return (
-    <Suspense fallback={<Fallback/>}>
-        <FlatList
-            data={data}
-            renderItem={({ item }) => <Item name={item.name} />}
-            keyExtractor={(item) => item.localAuthorityId.toString()}
-        />
+    <Suspense fallback={<Fallback />}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Item name={item.name} />}
+        keyExtractor={(item) => item.localAuthorityId.toString()}
+      />
     </Suspense>
   );
 };
