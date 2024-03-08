eabe6ead234e9cc5905a6e207710debb5f30fece
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-03-08 18:36:58 +0000

    Use counter not just boolean in useRefresh hook.

--- src/useRefresh.tsx
+++ src/useRefresh.tsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 // Connects https://reactnative.dev/docs/flatlist onRefresh()/refreshing with refetch()
 // returned by https://tanstack.com/query/v4/docs/framework/react/reference/useQuery
 export function useRefresh<T>(refetch: () => Promise<T>) {
+  const pendingRefreshCount = useRef(0);
   const [refreshing, setRefreshing] = useState(false);
 
   async function onRefresh() {
     setRefreshing(true);
+    pendingRefreshCount.current++;
     try {
       await refetch();
     } finally {
-      setRefreshing(false);
+      pendingRefreshCount.current--;
+      if (pendingRefreshCount.current === 0) {
+        setRefreshing(false);
+      }
     }
   }
 
