6416407ce731b71142ede0ef8d0c2390cbba7dd5
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-03-09 18:01:12 +0000

    Configure jest. Add react native testing library. Add unit test for useRefresh() hook.

---
+++ src/__tests__/useRefresh.test.tsx
@@ -0,0 +1,16 @@
+import { act, renderHook, waitFor } from "@testing-library/react-native";
+
+import { useRefresh } from "../useRefresh";
+
+describe("useRefresh", () => {
+  it("refetch resolves immediately", async () => {
+    const refetch = async () => {};
+    const { result } = renderHook(() => useRefresh(refetch));
+    expect(result.current.refreshing).toEqual(false);
+    await act(async () => {
+      await result.current.onRefresh();
+      expect(result.current.refreshing).toEqual(true);
+    });
+    waitFor(() => expect(result.current.refreshing).toEqual(false));
+  });
+});
