056b72213524587e2dc6f6776da5aace56856549
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-03-09 18:25:58 +0000

    Attempt to test multiple overlapping onRefresh() calls. Not working. Oddly jest records test as a pass with green tick, but then there's an error backtrace and jest has non-zero exit code.

--- src/__tests__/useRefresh.test.tsx
+++ src/__tests__/useRefresh.test.tsx
@@ -1,4 +1,5 @@
 import { act, renderHook, waitFor } from "@testing-library/react-native";
+//import { times } from "lodash";
 
 import { useRefresh } from "../useRefresh";
 
@@ -26,4 +27,26 @@ describe("useRefresh", () => {
     });
     waitFor(() => expect(result.current.refreshing).toEqual(false));
   });
+
+  // it("multiple onRefresh calls before refetch resolves", async () => {
+  //   let delay = 100;
+  //   const refetch = jest.fn(() => {
+  //     return new Promise<void>((resolve) => {
+  //       setTimeout(() => resolve(), delay);
+  //       delay *= 0.8;
+  //     });
+  //   });
+  //   const { result } = renderHook(() => useRefresh(refetch));
+  //   expect(refetch).toHaveBeenCalledTimes(0);
+  //   expect(result.current.refreshing).toEqual(false);
+  //   const numberOfRefreshes = 3;
+  //   await act(async () => {
+  //     times(numberOfRefreshes, async () => {
+  //       await result.current.onRefresh();
+  //       expect(result.current.refreshing).toEqual(true);
+  //     });
+  //   });
+  //   expect(refetch).toHaveBeenCalledTimes(numberOfRefreshes);
+  //   waitFor(() => expect(result.current.refreshing).toEqual(false));
+  // });
 });
