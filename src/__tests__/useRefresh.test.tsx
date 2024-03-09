476fc9899c6d910f302b04edf498fbcc74e39c1e
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-03-09 18:05:28 +0000

    Add variant of the useRefresh() test.

--- src/__tests__/useRefresh.test.tsx
+++ src/__tests__/useRefresh.test.tsx
@@ -13,4 +13,17 @@ describe("useRefresh", () => {
     });
     waitFor(() => expect(result.current.refreshing).toEqual(false));
   });
+
+  it("refetch resolves after 100ms", async () => {
+    const refetch = () => {
+      return new Promise<void>((resolve) => setTimeout(() => resolve(), 100));
+    };
+    const { result } = renderHook(() => useRefresh(refetch));
+    expect(result.current.refreshing).toEqual(false);
+    await act(async () => {
+      await result.current.onRefresh();
+      expect(result.current.refreshing).toEqual(true);
+    });
+    waitFor(() => expect(result.current.refreshing).toEqual(false));
+  });
 });
