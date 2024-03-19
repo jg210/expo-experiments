1e9dada076fd75743e35b248a0b4a6d86bcaed6c
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-03-19 15:34:07 +0000

    Improve useRefresh comments.

--- src/useRefresh.tsx
+++ src/useRefresh.tsx
@@ -3,7 +3,10 @@ import { useRef, useState } from "react";
 // Connects https://reactnative.dev/docs/flatlist onRefresh()/refreshing with refetch()
 // returned by https://tanstack.com/query/v4/docs/framework/react/reference/useQuery
 export function useRefresh<T>(refetch: () => Promise<T>) {
+  // The synchronous onRefresh() calls increment the counter, and the
+  // asynchronous refetch() Promise resolution decrements the counter.
   const pendingRefreshCount = useRef(0);
+  // True if and only if count is non-zero.
   const [refreshing, setRefreshing] = useState(false);
 
   async function onRefresh() {
