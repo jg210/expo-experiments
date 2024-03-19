1ff82a43c2d102222538e58dcb811c2a38add8db
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-03-19 15:24:49 +0000

    Add unit test for multiple refetch() promises resolved in order of creation.

--- src/__tests__/useRefresh.test.tsx
+++ src/__tests__/useRefresh.test.tsx
@@ -1,7 +1,7 @@
 import { act, renderHook, waitFor } from "@testing-library/react-native";
-//import { times } from "lodash";
 
 import { useRefresh } from "../useRefresh";
+import { times } from "lodash";
 
 describe("useRefresh", () => {
   it("refetch resolves immediately", async () => {
@@ -32,25 +32,42 @@ describe("useRefresh", () => {
     expect(refetch).toHaveBeenCalledTimes(1);
   });
 
-  // it("multiple onRefresh calls before refetch resolves", async () => {
-  //   let delay = 100;
-  //   const refetch = jest.fn(() => {
-  //     return new Promise<void>((resolve) => {
-  //       setTimeout(() => resolve(), delay);
-  //       delay *= 0.8;
-  //     });
-  //   });
-  //   const { result } = renderHook(() => useRefresh(refetch));
-  //   expect(refetch).toHaveBeenCalledTimes(0);
-  //   expect(result.current.refreshing).toEqual(false);
-  //   const numberOfRefreshes = 3;
-  //   await act(async () => {
-  //     times(numberOfRefreshes, async () => {
-  //       await result.current.onRefresh();
-  //       expect(result.current.refreshing).toEqual(true);
-  //     });
-  //   });
-  //   expect(refetch).toHaveBeenCalledTimes(numberOfRefreshes);
-  //   waitFor(() => expect(result.current.refreshing).toEqual(false));
-  // });
+  describe("multiple refetch() promises pending at same time", () => {
+    it("resolved in order of creation", async () => {
+      // Set up a refetch() function that stores resolve function for deferred resolution, and that
+      // uses jest.fn() so can track that this function is called.
+      const toResolve: (() => void)[] = [];
+      const refetch = jest.fn(() => {
+        return new Promise<void>((resolve, _reject) => {
+          toResolve.push(resolve);
+        });
+      });
+
+      // SUT
+      const { result } = renderHook(() => useRefresh(refetch));
+
+      // Nothing's happened yet...
+      expect(refetch).toHaveBeenCalledTimes(0);
+      expect(result.current.refreshing).toEqual(false);
+
+      // Set up some unresolved refreshes.
+      const n = 3;
+      await act(async () => {
+        times(n, async () => {
+          await result.current.onRefresh();
+          expect(result.current.refreshing).toEqual(true);
+        });
+      });
+      expect(refetch).toHaveBeenCalledTimes(3);
+      expect(result.current.refreshing).toEqual(true);
+
+      // Resolve all the promises...
+      await act(async () => {
+        toResolve.forEach((resolve) => resolve());
+      });
+
+      // ...then the refreshing state should become false.
+      expect(result.current.refreshing).toEqual(false);
+    });
+  });
 });
