11f1c6072b0f2379268e11029de14e5a9735c4c2
Jeremy Green <20728342+jg210@users.noreply.github.com>
2025-01-30 11:32:03 +0000

    Fix eslint warnings.

--- src/__tests__/useRefresh.test.tsx
+++ src/__tests__/useRefresh.test.tsx
@@ -1,7 +1,7 @@
 import { act, renderHook, waitFor } from "@testing-library/react-native";
+import { times } from "lodash";
 
 import { useRefresh } from "../useRefresh";
-import { times } from "lodash";
 
 describe("useRefresh", () => {
   it("refetch resolves immediately", async () => {
