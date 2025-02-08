4df601627a36e524492adb27ac36c1e055b21e45
Jeremy Green <20728342+jg210@users.noreply.github.com>
2025-02-08 17:13:01 +0000

    Move ignore code into custom hook.

---
+++ src/useLastPromise.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+
+// Ensures racing promises can't be reordered.
+//
+// Based on pattern from https://react.dev/learn/synchronizing-with-effects#fetching-data
+//
+// react-hooks/exhaustive-deps is configured with additionalHooks to check the deps.
+// It expects first and second args to look like useEffect's args, so promise and deps must be
+// first and second argument.
+//
+// promise - returns Promise that depends on just deps
+// deps - same as for useEffect
+// commit - given the value returned by promise unless there's a newer render in progress. If it depends on any values, they'd need to go in deps too.
+export function useLastPromise<T>(
+  promise: () => Promise<T>,
+  deps: React.DependencyList,
+  commit: (value: T) => void) {
+    useEffect(
+      () => {
+        let ignore = false;
+        promise().then(value => {
+          if (!ignore) {
+            commit(value);
+          }
+        });
+        return () => { ignore = true };
+      },
+      deps // eslint-disable-line react-hooks/exhaustive-deps
+    );
+};
+  
