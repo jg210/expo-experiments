9e063b13ed62b48cbe6c51ddbe6a90640f1316ba
Jeremy Green <20728342+jg210@users.noreply.github.com>
2025-02-26 20:29:27 +0000

    Increase test case timeout to 15s too.

--- src/__tests__/Authorities.test.tsx
+++ src/__tests__/Authorities.test.tsx
@@ -121,7 +121,7 @@ describe("Authorities", () => {
         })
 
         await assertUICorrect(fingerprint2, localAuthorities2);
-    });
+    }, 15000);
 
     // TODO test data loading state by mocking a slow network.
     // TODO test fingerprint loading state by mocking promise that doesn't resolve.
