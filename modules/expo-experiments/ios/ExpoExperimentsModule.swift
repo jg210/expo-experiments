3bd3591e949286cc461bb2fe4d7bbeddb6e71bea
Jeremy Green <20728342+jg210@users.noreply.github.com>
2025-03-16 16:51:04 +0000

    SHA256 implementation in ExpoExperimentsModule.swift.

--- modules/expo-experiments/ios/ExpoExperimentsModule.swift
+++ modules/expo-experiments/ios/ExpoExperimentsModule.swift
@@ -1,12 +1,31 @@
+import CryptoKit
 import ExpoModulesCore
+import Foundation
 
 public class ExpoExperimentsModule: Module {
   public func definition() -> ModuleDefinition {
     Name("ExpoExperiments")
 
     AsyncFunction("fingerprintAuthorities") { (authorities: [String], promise: Promise) in
-      promise.resolve("1234") // TODO implement fingerprinting
+      promise.resolve(fingerprintAuthorities(authorities))
     }
 
   }
+}
+
+private static let digestSeparator: Data = Data([0])
+
+private func fingerprintAuthorities(authorities: [String]) -> String {
+    var digest = SHA256()
+    for authority in authorities {
+        digest.update(data: authority.data(using: .utf8)!)
+        digest.update(data: digestSeparator)
+    }
+    digest.finalize().hexString
+}
+
+private extension Data {
+    var hexString: String {
+        return map { String(format: "%02x", $0) }.joined()
+    }
 }
