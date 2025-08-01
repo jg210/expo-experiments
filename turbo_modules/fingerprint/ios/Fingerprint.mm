1f1a34c3ada9c8aa34ada2512943d0885e3553e6
Jeremy Green <20728342+jg210@users.noreply.github.com>
2025-08-01 15:46:44 +0100

    iOS implementation and an end-to-end test.

--- turbo_modules/fingerprint/ios/Fingerprint.mm
+++ turbo_modules/fingerprint/ios/Fingerprint.mm
@@ -1,12 +1,37 @@
 #import "Fingerprint.h"
+#import <CommonCrypto/CommonDigest.h>
 
 @implementation Fingerprint
 RCT_EXPORT_MODULE()
 
-- (NSNumber *)multiply:(double)a b:(double)b {
-    NSNumber *result = @(a * b);
+RCT_REMAP_METHOD(fingerprintAuthorities,
+                 fingerprintAuthorities:(NSArray<NSString *> *)authorities
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+  if (![authorities isKindOfClass:[NSArray class]]) {
+    reject(@"invalid_argument", @"Expected an array of strings", nil);
+    return;
+  }
+  NSMutableData *data = [NSMutableData data];
+  for (NSString *authority in authorities) {
+    NSData *authorityData = [authority dataUsingEncoding:NSUTF8StringEncoding];
+    [data appendData:authorityData];
+    uint8_t nullByte = 0;
+    [data appendBytes:&nullByte length:1];
+  }
 
-    return result;
+  // Compute SHA-512
+  uint8_t hash[CC_SHA512_DIGEST_LENGTH];
+  CC_SHA512(data.bytes, (CC_LONG)data.length, hash);
+
+  // Convert to hex string
+  NSMutableString *hexString = [NSMutableString stringWithCapacity:CC_SHA512_DIGEST_LENGTH * 2];
+  for (int i = 0; i < CC_SHA512_DIGEST_LENGTH; i++) {
+    [hexString appendFormat:@"%02x", hash[i]];
+  }
+
+  resolve(hexString);
 }
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
