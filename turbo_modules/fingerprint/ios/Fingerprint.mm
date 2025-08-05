b17afea8d26b3159d4c98a426b97c6f9216e2d4e
Jeremy Green <20728342+jg210@users.noreply.github.com>
2025-08-05 20:06:57 +0100

    Comment out all of iOS turbo module fingerprintAuthorities().

--- turbo_modules/fingerprint/ios/Fingerprint.mm
+++ turbo_modules/fingerprint/ios/Fingerprint.mm
@@ -8,29 +8,29 @@ RCT_EXPORT_MODULE()
         resolver:(RCTPromiseResolveBlock)resolve
         rejecter:(RCTPromiseRejectBlock)reject
 {
-  if (![authorities isKindOfClass:[NSArray class]]) {
-    reject(@"invalid_argument", @"Expected an array of strings", nil);
-    return;
-  }
-  NSMutableData *data = [NSMutableData data];
-  for (NSString *authority in authorities) {
-    NSData *authorityData = [authority dataUsingEncoding:NSUTF8StringEncoding];
-    [data appendData:authorityData];
-    uint8_t nullByte = 0;
-    [data appendBytes:&nullByte length:1];
-  }
+  // if (![authorities isKindOfClass:[NSArray class]]) {
+  //   reject(@"invalid_argument", @"Expected an array of strings", nil);
+  //   return;
+  // }
+  // NSMutableData *data = [NSMutableData data];
+  // for (NSString *authority in authorities) {
+  //   NSData *authorityData = [authority dataUsingEncoding:NSUTF8StringEncoding];
+  //   [data appendData:authorityData];
+  //   uint8_t nullByte = 0;
+  //   [data appendBytes:&nullByte length:1];
+  // }
 
-  // Compute SHA-512
-  uint8_t hash[CC_SHA512_DIGEST_LENGTH];
-  CC_SHA512(data.bytes, (CC_LONG)data.length, hash);
+  // // Compute SHA-512
+  // uint8_t hash[CC_SHA512_DIGEST_LENGTH];
+  // CC_SHA512(data.bytes, (CC_LONG)data.length, hash);
 
-  // Convert to hex string
-  NSMutableString *hexString = [NSMutableString stringWithCapacity:CC_SHA512_DIGEST_LENGTH * 2];
-  for (int i = 0; i < CC_SHA512_DIGEST_LENGTH; i++) {
-    [hexString appendFormat:@"%02x", hash[i]];
-  }
+  // // Convert to hex string
+  // NSMutableString *hexString = [NSMutableString stringWithCapacity:CC_SHA512_DIGEST_LENGTH * 2];
+  // for (int i = 0; i < CC_SHA512_DIGEST_LENGTH; i++) {
+  //   [hexString appendFormat:@"%02x", hash[i]];
+  // }
 
-  resolve(hexString);
+  // resolve(hexString);
 }
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
