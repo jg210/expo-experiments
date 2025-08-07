e4a29c08b066cfc94cdcfabbf3ea11e419eb3d89
Jeremy Green <20728342+jg210@users.noreply.github.com>
2025-08-07 11:49:26 +0100

    $ npx @sentry/wizard@latest -i reactNative --saas --org jeremy-green --project expo-experiments
    ...version 6.1.2.

--- App.tsx
+++ App.tsx
@@ -3,8 +3,25 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Authorities } from "./src/Authorities";
 import { AppQueryClientProvider } from "./src/AppQueryClientProvider";
+import * as Sentry from "@sentry/react-native";
 
-export default function App() {
+Sentry.init({
+  dsn: "__TODO_SENTRY_DSN__",
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function App() {
   return (
     <SafeAreaView style={styles.container}>
       <AppQueryClientProvider>
@@ -12,7 +29,7 @@ export default function App() {
       </AppQueryClientProvider>
     </SafeAreaView>
   );
-}
+});
 
 const styles = StyleSheet.create({
   container: {
