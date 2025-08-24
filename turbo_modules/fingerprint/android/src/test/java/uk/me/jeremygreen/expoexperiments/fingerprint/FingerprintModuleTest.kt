72b88e2c91333fe8ba214f0e0771dc55473a4f58
Jeremy Green <20728342+jg210@users.noreply.github.com>
2025-08-24 22:44:47 +0100

    Add a failing unit test to FingerprintModuleTest (to make sure CI builds test, runs it and fails if test breaks).

---
+++ turbo_modules/fingerprint/android/src/test/java/uk/me/jeremygreen/expoexperiments/fingerprint/FingerprintModuleTest.kt
@@ -0,0 +1,13 @@
+package uk.me.jeremygreen.expoexperiments.fingerprint
+
+import org.junit.Assert
+import org.junit.Test
+
+class FingerprintModuleTest {
+
+    @Test
+    fun `it fails`() {
+        Assert.fail("it fails")
+    }
+
+}
