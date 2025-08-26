a53b0a044d5a2def6f39cd652f5ff6fed23c5647
Jeremy Green <20728342+jg210@users.noreply.github.com>
2025-08-26 12:00:26 +0100

    Android unit tests for FingerprintModule.

--- turbo_modules/fingerprint/android/src/test/java/uk/me/jeremygreen/expoexperiments/fingerprint/FingerprintModuleTest.kt
+++ turbo_modules/fingerprint/android/src/test/java/uk/me/jeremygreen/expoexperiments/fingerprint/FingerprintModuleTest.kt
@@ -6,8 +6,35 @@ import org.junit.Test
 class FingerprintModuleTest {
 
     @Test
-    fun `it fails`() {
-        Assert.fail("it fails")
+    fun `fingerprintAuthorities empty list`() {
+        Assert.assertEquals(
+            "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e",
+            FingerprintModule.fingerprintAuthorities(listOf())
+        )
+    }
+
+    @Test
+    fun `fingerprintAuthorities one element`() {
+        Assert.assertEquals(
+            "5c2ca3d50f46ece6066c53bd1a490cbe5f72d2738ae9417332e91e5c3f75205c639d71a9a41d67d965fa137dddf439e0ab9443a6ea44915e90d8b5b566d1c076",
+            FingerprintModule.fingerprintAuthorities(listOf("a"))
+        )
+    }
+
+    @Test
+    fun `fingerprintAuthorities separator`() {
+        Assert.assertNotEquals(
+            FingerprintModule.fingerprintAuthorities(listOf("a", "bc")),
+            FingerprintModule.fingerprintAuthorities(listOf("ab", "c"))
+        )
+    }
+
+    @Test
+    fun `fingerprintAuthorities permutation`() {
+        Assert.assertNotEquals(
+            FingerprintModule.fingerprintAuthorities(listOf("a", "b")),
+            FingerprintModule.fingerprintAuthorities(listOf("b", "a"))
+        )
     }
 
 }
