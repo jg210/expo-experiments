84fc8883dea9ac8f67d74b56a128e30d07b2631c
Jeremy Green <20728342+jg210@users.noreply.github.com>
2025-08-26 13:11:47 +0100

    More android unit tests for turbo module.

--- turbo_modules/fingerprint/android/src/test/java/uk/me/jeremygreen/expoexperiments/fingerprint/FingerprintModuleTest.kt
+++ turbo_modules/fingerprint/android/src/test/java/uk/me/jeremygreen/expoexperiments/fingerprint/FingerprintModuleTest.kt
@@ -1,25 +1,26 @@
 package uk.me.jeremygreen.expoexperiments.fingerprint
 
-import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.JavaOnlyArray
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.bridge.WritableArray
-import io.mockk.Runs
-import io.mockk.every
-import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Assert
 import org.junit.Test
+
+private const val FINGERPRINT_EMPTY =
+    "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"
+
+private const val FINGERPRINT_ONE_ELEMENT =
+    "5c2ca3d50f46ece6066c53bd1a490cbe5f72d2738ae9417332e91e5c3f75205c639d71a9a41d67d965fa137dddf439e0ab9443a6ea44915e90d8b5b566d1c076"
 
 class FingerprintModuleTest {
 
     @Test
     fun `fingerprintAuthorities empty list`() {
         Assert.assertEquals(
-            "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e",
+            FINGERPRINT_EMPTY,
             FingerprintModule.fingerprintAuthorities(listOf())
         )
     }
@@ -27,7 +28,7 @@ class FingerprintModuleTest {
     @Test
     fun `fingerprintAuthorities one element`() {
         Assert.assertEquals(
-            "5c2ca3d50f46ece6066c53bd1a490cbe5f72d2738ae9417332e91e5c3f75205c639d71a9a41d67d965fa137dddf439e0ab9443a6ea44915e90d8b5b566d1c076",
+            FINGERPRINT_ONE_ELEMENT,
             FingerprintModule.fingerprintAuthorities(listOf("a"))
         )
     }
@@ -48,16 +49,24 @@ class FingerprintModuleTest {
         )
     }
 
-    @Test
-    fun `fingerprintAuthorities promise empty list` () {
+    private fun assertFingerprintAuthoritiesResolves(
+        expectedFingerprint: String,
+        vararg authorities: String
+    ) {
         val reactApplicationContext = mockk<ReactApplicationContext>()
         val fingerprintModule = FingerprintModule(reactApplicationContext)
         val promise = mockk<Promise>(relaxed = true)
-        val authorities: WritableArray = JavaOnlyArray.of()
-        fingerprintModule.fingerprintAuthorities(authorities, promise)
-        verify(exactly = 1) { promise.resolve("cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e")}
+        val authoritiesWritableArray: WritableArray = JavaOnlyArray.from(authorities.toList())
+        fingerprintModule.fingerprintAuthorities(authoritiesWritableArray, promise)
+        verify(exactly = 1) { promise.resolve(expectedFingerprint) }
         verify(exactly = 0) { promise.reject(any<String>()) }
         verify(exactly = 0) { promise.reject(any<Throwable>()) }
     }
+
+    @Test
+    fun `fingerprintAuthorities promise empty list` () = assertFingerprintAuthoritiesResolves(FINGERPRINT_EMPTY)
+
+    @Test
+    fun `fingerprintAuthorities promise one element` () = assertFingerprintAuthoritiesResolves(FINGERPRINT_ONE_ELEMENT, "a")
 
 }
