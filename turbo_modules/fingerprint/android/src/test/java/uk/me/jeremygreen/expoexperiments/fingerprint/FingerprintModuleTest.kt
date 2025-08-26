80849b02655e10b88028eb2f08224a47a41a91c7
Jeremy Green <20728342+jg210@users.noreply.github.com>
2025-08-26 13:02:22 +0100

    Add a first unit test for the android Promise turbo module API.

--- turbo_modules/fingerprint/android/src/test/java/uk/me/jeremygreen/expoexperiments/fingerprint/FingerprintModuleTest.kt
+++ turbo_modules/fingerprint/android/src/test/java/uk/me/jeremygreen/expoexperiments/fingerprint/FingerprintModuleTest.kt
@@ -1,5 +1,16 @@
 package uk.me.jeremygreen.expoexperiments.fingerprint
 
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.JavaOnlyArray
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReadableArray
+import com.facebook.react.bridge.WritableArray
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Assert
 import org.junit.Test
 
@@ -35,6 +46,18 @@ class FingerprintModuleTest {
             FingerprintModule.fingerprintAuthorities(listOf("a", "b")),
             FingerprintModule.fingerprintAuthorities(listOf("b", "a"))
         )
+    }
+
+    @Test
+    fun `fingerprintAuthorities promise empty list` () {
+        val reactApplicationContext = mockk<ReactApplicationContext>()
+        val fingerprintModule = FingerprintModule(reactApplicationContext)
+        val promise = mockk<Promise>(relaxed = true)
+        val authorities: WritableArray = JavaOnlyArray.of()
+        fingerprintModule.fingerprintAuthorities(authorities, promise)
+        verify(exactly = 1) { promise.resolve("cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e")}
+        verify(exactly = 0) { promise.reject(any<String>()) }
+        verify(exactly = 0) { promise.reject(any<Throwable>()) }
     }
 
 }
